dda4da6a19dd401980011e6b7675da9700be2426
laroccacharly <larocca.charly@gmail.com>
2025-04-19 09:16:01 -0400

    update main

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 
 from src.canada_ui import get_graph_data
-
+from src.config import Config
 def main():
-    get_graph_data(30)
+    config = Config(
+        max_node_count=30
+    )
+    get_graph_data(config)
 
 if __name__ == "__main__":
     main()
